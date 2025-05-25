aae91e05edb2540e25d10cd9f2a0025bbd20520f
justacow224 <justacow224@gmail.com>
2025-05-25 22:26:50 +0700

    implement activate (not tested yet)

--- src/main/Domain/Controller/CommandModel.py
+++ src/main/Domain/Controller/CommandModel.py
@@ -1,9 +1,13 @@
 from abc import ABC, abstractmethod
 from Infrastructure.Logging import write_log
+from src.main.Domain.Retriever.MQTTManagerModel import MQTTManager
+
 class ActivationCommand(ABC):
     def __init__(self, DeviceInfo, Parameter: dict):
         self.DeviceInfo = DeviceInfo #ACCESS NAME
         self.Parameter = Parameter
+        self.mqttmanager = MQTTManager("mqtt.ohstem.vn", 1883, "Tuluu", "")
+
     @abstractmethod
     def run(self):
         pass
@@ -11,8 +15,24 @@ class ActivationCommand(ABC):
 class ActivatePump(ActivationCommand):
     def run(self):
         write_log(f"device {self.DeviceInfo} activate water pump")
+        # Publish to the correct topic based on the parameter key
+        for key, value in self.Parameter.items():
+            if key == "Pump_1":
+                topic = "Tuluu/feeds/V10"
+                self.mqttmanager.client.publish(topic, value)
+            elif key == "Pump_2":
+                topic = "Tuluu/feeds/V11"
+                self.mqttmanager.client.publish(topic, value)
+            else:
+                continue
+            
 
 class ActivateFan(ActivationCommand):
     def run(self):
         write_log(f"device {self.DeviceInfo} activate Fan")
+            # Publish to the correct topic based on the parameter key
+        for key, value in self.Parameter.items():
+            if key == "Fan":
+                topic = "Tuluu/feeds/V12"
+                self.mqttmanager.client.publish(topic, value)
 
